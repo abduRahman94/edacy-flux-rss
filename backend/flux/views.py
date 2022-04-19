c16c1f0b4fbdd2fc0cecb7111c0ee5c2eff63a98
Abdou SEYE <abdou.seye@gsietechnology.com>
2022-04-19 13:06:15 +0000

    Ajout package requests_xml

--- backend/flux/views.py
+++ backend/flux/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from requests_xml import XMLSession()
 # Create your views here.
+
+
+def get_flux(request):
+    session = XMLSession()
+    link = 'https://www.lemonde.fr/rss/en_continu.xml'
+    response = session.get(link)
+    links = response.xml.links
+    return render(request, 'flux/test_flux.html', context={'data': links})
