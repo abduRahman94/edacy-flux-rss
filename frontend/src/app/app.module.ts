c2fe02486571eb69b95c2b905c2dd41f6a9a0715
Abdou SEYE <abdou.seye@gsietechnology.com>
2022-04-21 08:46:01 +0000

    Feat: Pagination frontend implemented

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FluxComponent } from './flux/flux.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FluxComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/web-api.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Response } from './flux/flux';
+
+let headers = new HttpHeaders({
+  'Content-Type': 'application/json',
+});
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebAPIService {
+  api_url = 'http://127.0.0.1:8000/api/flux/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getFluxMany() {
+    return this.httpClient.get< Response [] >(this.api_url, {'headers': headers});
+  }
+
+  paginate(pageNumber: number){
+    let url = this.api_url + '?page=' + pageNumber;
+    return this.httpClient.get< Response [] >(url, {'headers': headers});
+  }
+
+  paginateNextPrevious(urlString: string){
+    if(urlString != null){
+      return this.httpClient.get< Response [] >(urlString, {'headers': headers});
+    }
+    else{
+      console.log(window.location)
+      return '';
+    }
+  }
+
+}
